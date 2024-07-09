e48eb46a6351964819d16208750376d69b772f6b
Tsuguko35 <carpio.johnjazpher.dc.3188@gmail.com>
2024-07-09 12:56:24 +0800

    messenger style

--- client/src/components/FacebookMsg.js
+++ client/src/components/FacebookMsg.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { FaFacebookMessenger } from "react-icons/fa";
+import { GetWindowWidth } from "../utils";
 
 function FacebookMsg() {
+  const windowWidth = GetWindowWidth();
   return (
     <a
       title="Send a message"
-      className="messenger"
-      href="https://m.me/281503175054414"
+      className={`messenger ${windowWidth < 768 ? "mobile" : ""}`}
+      href="https://m.me/303695626163278"
       target="_blank"
     >
+      {windowWidth < 768 ? "" : "Message Us!"}
       <FaFacebookMessenger />
     </a>
   );
